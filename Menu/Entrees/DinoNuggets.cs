c458ff86ce8e6238a48e12b844e2258ce844e2cf
Taylor Wilcox <tjwilcox@users.campus>
2019-09-09 17:14:42 -0500

    Added Brontowurst + edit needs edit

---
+++ Menu/Entrees/DinoNuggets.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Entrees
+{
+    public class DinoNuggets
+    {
+        private int nuggetCount = 6;
+        public double Price { get; set; }
+
+        public double Calories { get; set; }
+
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>() { "Chicken Nugget" };
+                return ingredients;
+            }
+        }
+
+        public DinoNuggets()
+        {
+            this.Price = 4.25;
+            this.Calories = 354;
+        }
+
+        public void AddNugget()
+        {
+            this.nuggetCount++;
+            this.Price += 0.25;
+            this.Calories += 59;
+        }
+    }
+}
